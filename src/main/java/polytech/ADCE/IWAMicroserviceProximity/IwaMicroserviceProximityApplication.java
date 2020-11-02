c9463016d4987ba6aecf8bcf9a99471f99acc2df
pepitosama <etienne.saimond@gmail.com>
2020-11-02 14:49:59 +0100

    First Commit

---
+++ src/main/java/polytech/ADCE/IWAMicroserviceProximity/IwaMicroserviceProximityApplication.java
@@ -0,0 +1,13 @@
+package polytech.ADCE.IWAMicroserviceProximity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaMicroserviceProximityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwaMicroserviceProximityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/polytech/ADCE/IWAMicroserviceProximity/IwaMicroserviceProximityApplicationTests.java
@@ -0,0 +1,13 @@
+package polytech.ADCE.IWAMicroserviceProximity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaMicroserviceProximityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
